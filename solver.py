4c7ddafa96dd2a4f824e11fad049023fe8890136
Carlos Gomez <carlos@atl-software.net>
2018-02-12 16:46:51 -0400

    class rename

--- solver.py
+++ solver.py
@@ -2,7 +2,7 @@ import queue
 import sys
 
 
-class SearchAlgorithms:
+class Solver:
     def bfs(initialState, goalState):
         frontier = queue.Queue()
         frontier.put(initialState)
@@ -12,7 +12,9 @@ class SearchAlgorithms:
             explored.append(state)
             if state == goalState:
                 return state
+
     def dfs(initialState, goalTest):
         pass
+
     def ucs(initialState, goalTest):
         pass
