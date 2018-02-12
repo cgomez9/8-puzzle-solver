b97e0cc03c1ded460bb6b78d945037912828f111
Carlos Gomez <carlos@atl-software.net>
2018-02-12 13:03:58 -0400

    bfs basic algorithm

--- SearchAlgorithms.py
+++ SearchAlgorithms.py
@@ -1,8 +1,17 @@
 import queue
+import sys
 
 class SearchAlgorithms:
-    def bfs(initialState, goalTest):
-        pass
+    def bfs(initialState, goalState):
+        frontier = queue.Queue()
+        frontier.put(initialState)
+        explored = []
+        while !frontier.empty():
+            state = frontier.get()
+            explored.append(state)
+            if state === goalState:
+                return state
+
     def dfs(initialState, goalTest):
         pass
     def ucs(initialState, goalTest):
