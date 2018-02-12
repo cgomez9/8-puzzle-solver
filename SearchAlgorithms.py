49db64bb6569fbfa28819fcd0c2033da3c5fce22
Carlos Gomez <carlos@atl-software.net>
2018-02-12 12:38:02 -0400

    Initial Commit

---
+++ SearchAlgorithms.py
@@ -0,0 +1,9 @@
+import queue
+
+class SearchAlgorithms:
+    def bfs(initialState, goalTest):
+        pass
+    def dfs(initialState, goalTest):
+        pass
+    def ucs(initialState, goalTest):
+        pass
