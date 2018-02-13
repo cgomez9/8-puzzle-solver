c23c062bb19a6c1e5d7b8095d76faeeadec27b76
Carlos Gomez <carlos@atl-software.net>
2018-02-13 13:04:14 -0400

    Puzzle constructor and isSolved method

--- puzzle.py
+++ puzzle.py
@@ -2,9 +2,22 @@ import sys
 
 
 class Puzzle:
+    table = []
     PUZZLE_DIMENSION = 3
     SOLUTION_STATE = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8]
     ]
+
+    def __init__(self, initState):
+        initStateIndex = 0
+        for row in range(0, self.PUZZLE_DIMENSION):
+            for column in range(0, self.PUZZLE_DIMENSION):
+                self.table[row][column] = initState[initStateIndex]
+                initStateIndex += 1
+
+    def isSolved(self,currentState):
+        self.table == self.SOLUTION_STATE
+
+            
