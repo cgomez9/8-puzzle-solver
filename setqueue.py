0a4f32c46a07feb7e2de33c46a75b5e04a1b8117
Carlos Gomez <salos_12@hotmail.com>
2018-02-17 22:35:36 -0400

    FIFO Queue

--- setqueue.py
+++ setqueue.py
@@ -1,9 +1,12 @@
+from orderedset import OrderedSet
 import queue
 
 class SetQueue(queue.Queue):
     def _init(self, maxsize):
-        self.queue = set()
+        self.queue = OrderedSet()
+
     def _put(self, item):
         self.queue.add(item)
+
     def _get(self):
-        return self.queue.pop()
+        return self.queue.pop(False)
