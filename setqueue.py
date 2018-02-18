7485e2ca04997535a15d4547dab1741102162b16
Carlos Gomez <salos_12@hotmail.com>
2018-02-17 21:45:16 -0400

    Set queue and stack classes

---
+++ setqueue.py
@@ -0,0 +1,9 @@
+import queue
+
+class SetQueue(queue.Queue):
+    def _init(self, maxsize):
+        self.queue = set()
+    def _put(self, item):
+        self.queue.add(item)
+    def _get(self):
+        return self.queue.pop()
